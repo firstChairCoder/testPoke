010ef0aee8ffc69ec1717e1bc36b6bfac7183944
firstChairCoder <md1qq7@gmail.com>
2023-03-17 19:36:02 +0100

    Initial commit

---
+++ src/global/types.ts
@@ -0,0 +1,90 @@
+export enum PokemonTypes {
+  NORMAL = "Normal",
+  FIRE = "Fire",
+  WATER = "Water",
+  ELECTRIC = "Electric",
+  GRASS = "Grass",
+  ICE = "Ice",
+  FIGHTING = "Fighting",
+  POISON = "Poison",
+  GROUND = "Ground",
+  FLYING = "Flying",
+  PSYCHIC = "Psychic",
+  BUG = "Bug",
+  ROCK = "Rock",
+  GHOST = "Ghost",
+  DRAGON = "Dragon",
+  DARK = "Dark",
+  STEEL = "Steel",
+  FAIRY = "Fairy",
+}
+
+export const stringToPokemonType = {
+  Normal: PokemonTypes.NORMAL,
+  Fire: PokemonTypes.FIRE,
+  Water: PokemonTypes.WATER,
+  Electric: PokemonTypes.ELECTRIC,
+  Grass: PokemonTypes.GRASS,
+  Ice: PokemonTypes.ICE,
+  Fighting: PokemonTypes.FIGHTING,
+  Poison: PokemonTypes.POISON,
+  Ground: PokemonTypes.GROUND,
+  Flying: PokemonTypes.FLYING,
+  Psychic: PokemonTypes.PSYCHIC,
+  Bug: PokemonTypes.BUG,
+  Rock: PokemonTypes.ROCK,
+  Ghost: PokemonTypes.GHOST,
+  Dragon: PokemonTypes.DRAGON,
+  Dark: PokemonTypes.DARK,
+  Steel: PokemonTypes.STEEL,
+  Fairy: PokemonTypes.FAIRY,
+};
+
+export const stringToType = (typeString?: string) => {
+  if (!typeString) {
+    return undefined;
+  }
+  return Object.values(PokemonTypes).find(
+    (t) => t.toLowerCase() === typeString.toLowerCase()
+  );
+};
+
+export type Pokemon = {
+  id: number;
+  name: string;
+  weight: number;
+  height: number;
+  baseExperience: number;
+  types: Type[];
+  moves: { move: Move[] };
+  stats: Stats[];
+  abilities: { ability: Ability; isHidden: boolean }[];
+};
+
+export type Move = {
+  name: string;
+  url: string;
+};
+
+type Stats = {
+  base_stat: number;
+  stat: { name: string };
+};
+
+export type Ability = {
+  name: string;
+  url: string;
+};
+
+export type Type = {
+  slot: number;
+  type: { name: string; url: string };
+};
+
+export type Generation = {
+  id: number;
+  region: string;
+  games?: string[];
+  firstPokemonId: number;
+  lastPokemonId: number;
+};
---
+++ src/themes/index.ts
@@ -0,0 +1,109 @@
+import { Dimensions as NativeDimensions } from "react-native";
+import { PokemonTypes, stringToType } from "../global/types";
+
+const SCREEN_WIDTH = NativeDimensions.get("window").width;
+const SCREEN_HEIGHT = NativeDimensions.get("window").height;
+const BASE_UNIT = 4;
+
+type Dimensions = {
+  height: number;
+  width: number;
+};
+
+enum IconSizes {
+  SMALL = 24,
+  MEDIUM = 32,
+  LARGE = 64,
+  XXL = 100,
+}
+
+const typeColorsBackground = new Map([
+  [PokemonTypes.NORMAL, "#B5B9C4"],
+  [PokemonTypes.FIRE, "#FFA756"],
+  [PokemonTypes.WATER, "#58ABF6"],
+  [PokemonTypes.ELECTRIC, "#F2CB55"],
+  [PokemonTypes.GRASS, "#8BBE8A"],
+  [PokemonTypes.ICE, "#91D8DF"],
+  [PokemonTypes.FIGHTING, "#EB4971"],
+  [PokemonTypes.POISON, "#9F6E97"],
+  [PokemonTypes.GROUND, "#F78551"],
+  [PokemonTypes.FLYING, "#83A2E3"],
+  [PokemonTypes.PSYCHIC, "#FF6568"],
+  [PokemonTypes.BUG, "#8BD674"],
+  [PokemonTypes.ROCK, "#D4C294"],
+  [PokemonTypes.GHOST, "#8571BE"],
+  [PokemonTypes.DRAGON, "#7383B9"],
+  [PokemonTypes.DARK, "#6F6E78"],
+  [PokemonTypes.STEEL, "#4C91B2"],
+  [PokemonTypes.FAIRY, "#EBA8C3"],
+]);
+
+const typeColors = new Map([
+  [PokemonTypes.NORMAL, "#9DA0AA"],
+  [PokemonTypes.FIRE, "#FD7D24"],
+  [PokemonTypes.WATER, "#4A90DA"],
+  [PokemonTypes.ELECTRIC, "#EED535"],
+  [PokemonTypes.GRASS, "#62B957"],
+  [PokemonTypes.ICE, "#61CEC0"],
+  [PokemonTypes.FIGHTING, "#D04164"],
+  [PokemonTypes.POISON, "#A552CC"],
+  [PokemonTypes.GROUND, "#DD7748"],
+  [PokemonTypes.FLYING, "#748FC9"],
+  [PokemonTypes.PSYCHIC, "#EA5D60"],
+  [PokemonTypes.BUG, "#8CB230"],
+  [PokemonTypes.ROCK, "#BAAB82"],
+  [PokemonTypes.GHOST, "#556AAE"],
+  [PokemonTypes.DRAGON, "#0F6AC0"],
+  [PokemonTypes.DARK, "#58575F"],
+  [PokemonTypes.STEEL, "#417D9A"],
+  [PokemonTypes.FAIRY, "#ED6EC7"],
+]);
+
+enum COLORS {
+  dark = "#FFFFFF",
+  cadet = "#586f7c",
+  ice = "#B8DBD9",
+  ghost = "#f4f4f9",
+  carbon = "#444444",
+  darkCarbon = "#E8E8E8",
+  white = "white",
+  platinum = "#E5E4E2",
+}
+
+const getBackgroundTypeColor = (typeString: string | undefined) => {
+  const type = stringToType(typeString);
+  return type ? typeColorsBackground.get(type) : theme.palette.white;
+};
+
+const getTypeColor = (typeString: string | undefined) => {
+  const type = stringToType(typeString);
+  return type ? typeColors.get(type) : theme.palette.white;
+};
+
+class Theme {
+  palette: typeof COLORS;
+  typePalette;
+  typePaletteBackground;
+  dimensions: Dimensions;
+  iconSize: typeof IconSizes;
+  getBackgroundTypeColor: (
+    typeString: string | undefined
+  ) => string | undefined;
+  getTypeColor: (typeString: string | undefined) => string | undefined;
+
+  constructor() {
+    this.palette = { ...COLORS };
+    this.dimensions = { height: SCREEN_HEIGHT, width: SCREEN_WIDTH };
+    this.iconSize = { ...IconSizes };
+    this.typePaletteBackground = typeColorsBackground;
+    this.typePalette = typeColors;
+    this.getBackgroundTypeColor = getBackgroundTypeColor;
+    this.getTypeColor = getTypeColor;
+  }
+
+  spacing(multiplier: number) {
+    return BASE_UNIT * multiplier;
+  }
+}
+
+export const theme = new Theme();
